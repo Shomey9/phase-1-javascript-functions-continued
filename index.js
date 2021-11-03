ec135009144f2fe998b080136b9ee2448e1e515d
Muhammad Rendusara <hadyanrendusara@yahoo.com>
2021-11-02 23:54:53 -0500

    commit tree 8996e665e9aa6e07973fe143b3e0e4bb21afce23

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity) {
+    switch (activity) {
+        case "bathe my dog": 
+            activity = `This Saturday, I want to bathe my dog!`;
+            return activity;
+        default: 
+            activity = `This Saturday, I want to roller-skate!`
+            console.log(activity);
+            return activity;
+    }
+}
+
+const mondayWork = function(work) {
+    switch (work) {
+        case 'work from home':
+            work = 'This Monday, I will work from home.';
+            return work;
+        default:
+            work = 'This Monday, I will go to the office.';
+            return work;
+    }
+}
+
+function wrapAdjective(adjective = "*") {
+    const innerFunction = function (endStatement = "special") {
+        return `You are ${adjective}${endStatement}${adjective}!`
+    }
+    return innerFunction;
+}
+wrapAdjective()("a hard worker");
+
+
